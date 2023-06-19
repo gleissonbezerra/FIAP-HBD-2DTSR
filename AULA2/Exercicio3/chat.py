fd56ca09d17133841d8c9a9e41c9245f06376288
Gleisson Bezerra <gbezerra@microsoft.com>
2023-06-19 05:28:50 -0300

    Aula 02

---
+++ AULA2/Exercicio3/chat.py
@@ -0,0 +1,32 @@
+import openai
+import os
+
+# Set up the model and prompt
+model_engine = "text-davinci-003"
+
+openai.api_key = "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"
+
+while True:
+    
+    prompt = input('PROMPT: ')
+
+    if 'sair' in prompt:
+        break
+
+    # Generate a response
+    # given the most recent context (4096 characters)
+    # continue the text up to 2048 tokens ~ 8192 charaters
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    # extracting useful part of response
+    response = completion.choices[0].text
+    
+    # printing response
+    print(response)
---
+++ AULA2/Exercicio3/readme.txt
@@ -0,0 +1,7 @@
+1. Go to OpenAI API (https://openai.com/blog/openai-api)
+2. Click on signup
+3. Complete SignUp with google or email or microsoft
+4. Click on Personal and then click on View API keys
+5. Now, click on ‘Create new secret key‘ and copy the secret key
+
+pip install --upgrade openai
